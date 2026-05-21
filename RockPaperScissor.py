433f9a89d7baf950adced7fd4a6c594828a78931
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-05-21 15:30:01 +0530

    Refactor score tracking and menu options

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -7,10 +7,10 @@ Full_Name = " ".join(w.capitalize() for w in Full_Name.split())
 Name = Full_Name.split(" ")
 Surname = Name[-1].capitalize()
 Main_Name = Name[0]
-Your_Overall_Score = 0
+Scores = {"Match Won" : 0, "Match Lost" : 0, "Match Drawn" : 0, "Total Score" : 0, "Round Won" : 0, "Round Lost" : 0, "Round Drawn" : 0}
 print(f"Hello, {Surname}. Nice to meet you.\n")
 while True:
-    Menu = int(input(f"To play three times: Enter *1*\nTo play five times : Enter *2*\nTo escape the game : Enter *3*\n"))
+    Menu = int(input(f" +--------------------------------+\n | To play three times: Enter [1] |\n | To play five times : Enter [2] |\n | To View Score      : Enter [3] | \n | To escape the game : Enter [4] |\n +--------------------------------+\n"))
     
     if Menu == 1:
         Your_score = 0
@@ -24,39 +24,52 @@ while True:
             if User_choice == "Rock":
                 if Computer_choice == User_choice:
                     print("It's A Tie!")
+                    Scores["Round Drawn"] += 1
                 elif Computer_choice == "Scissor":
                     print("You Won!")
                     Your_score += 1
+                    Scores["Round Won"] += 1
                 elif Computer_choice == "Paper":
                     print("You Lose!")
                     Computer_score += 1
+                    Scores["Round Lost"] += 1
             elif User_choice == "Paper":
                 if Computer_choice == User_choice:
                     print("It's A Tie!")
+                    Scores["Round Drawn"] += 1
                 elif Computer_choice == "Rock":
                     print("You Won!")
                     Your_score += 1
+                    Scores["Round Won"] += 1
                 elif Computer_choice == "Scissor":
                     print("You Lose!")
                     Computer_score += 1
+                    Scores["Round Lost"] += 1
             elif User_choice == "Scissor":
                 if Computer_choice == User_choice:
                     print("It's A Tie!")
+                    Scores["Round Drawn"] += 1
                 elif Computer_choice == "Paper":
                     print("You Won!")
                     Your_score += 1
+                    Scores["Round Won"] += 1
                 elif Computer_choice == "Rock":
                     print("You Lose!")
                     Computer_score += 1
+                    Scores["Round Lost"] += 1
             else:
                 print("Only Rock/Paper/Scissor are allowed.")
         if Your_score > Computer_score:
             print(f"You Won This Match {Main_Name}! By the score of {Your_score} to {Computer_score}")
-            Your_Overall_Score += 1
+            Scores["Match Won"] += 1
+            Scores["Total Score"] += 1
         elif Your_score < Computer_score:
             print(f"You Lost This Match {Main_Name}! By the score of {Your_score} to {Computer_score}")
+            Scores["Match Lost"] += 1
+            Scores["Total Score"] -= 1
         else:
             print(f"It's a tie {Main_Name}! By the score of {Your_score} to {Computer_score}")
+            Scores["Match Drawn"] += 1
     elif Menu == 2:
         Your_score = 0
         Computer_score = 0
@@ -69,41 +82,58 @@ while True:
             if User_choice == "Rock":
                 if Computer_choice == User_choice:
                     print("It's A Tie!")
+                    Scores["Round Drawn"] += 1
                 elif Computer_choice == "Scissor":
                     print("You Won!")
                     Your_score += 1
+                    Scores["Round Won"] += 1
                 elif Computer_choice == "Paper":
                     print("You Lose!")
                     Computer_score += 1
+                    Scores["Round Lost"] += 1
             elif User_choice == "Paper":
                 if Computer_choice == User_choice:
                     print("It's A Tie!")
+                    Scores["Round Drawn"] += 1
                 elif Computer_choice == "Rock":
                     print("You Won!")
                     Your_score += 1
+                    Scores["Round Won"] += 1
                 elif Computer_choice == "Scissor":
                     print("You Lose!")
                     Computer_score += 1
+                    Scores["Round Lost"] += 1
             elif User_choice == "Scissor":
                 if Computer_choice == User_choice:
                     print("It's A Tie!")
+                    Scores["Round Drawn"] += 1
                 elif Computer_choice == "Paper":
                     print("You Won!")
                     Your_score += 1
+                    Scores["Round Won"] += 1
                 elif Computer_choice == "Rock":
                     print("You Lose!")
                     Computer_score += 1
+                    Scores["Round Lost"] += 1
             else:
                 print("Only Rock/Paper/Scissor are allowed.")
         if Your_score > Computer_score:
             print(f"You Won This Match {Main_Name}! By the score of {Your_score} to {Computer_score}")
-            Your_Overall_Score += 1
+            Scores["Match Won"] += 1
+            Scores["Total Score"] += 1
         elif Your_score < Computer_score:
             print(f"You Lost This Match {Main_Name}! By the score of {Your_score} to {Computer_score}")
+            Scores["Match Lost"] += 1
+            Scores["Total Score"] -= 1
         else:
             print(f"It's a tie {Main_Name}! By the score of {Your_score} to {Computer_score}")
+            Scores["Match Drawn"] += 1
     elif Menu == 3:
+        #print(Scores)
+        for i, Score in Scores.items():
+            print(f"{i} : {Score}")
+    elif Menu == 4:
         break
     else:
-        print("Only 1/2/3 are allowed.")
-print(f"Total No. of Matches {Full_Name} have won: {Your_Overall_Score}")
+        print("Only 1/2/3/4 are allowed.")
+print(f"Total No. of Matches {Full_Name} have won: {Scores["Match Won"]}")
